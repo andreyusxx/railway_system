dd82747746e8a02120456c1bc9a09fdd561e241b
Шарагін Андрій <andrijsaragin@gmail.com>
2026-05-04 10:37:15 +0300

    feat: complete lab 14, finalize db connection and jpa repositories

--- src/main/java/ua/com/kisit/railwaysystem/repository/UserRepository.java
+++ src/main/java/ua/com/kisit/railwaysystem/repository/UserRepository.java
@@ -6,5 +6,6 @@ import ua.com.kisit.railwaysystem.entity.User;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    // Кастомний метод для пошуку користувача за логіном
     Optional<User> findByUsername(String username);
 }
